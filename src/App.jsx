a556ea5389a5291918d57cc9c18b73b8cb62c4da
Juan <saraviamartin16@hotmail.com>
2023-07-03 14:02:06 -0300

    fetching de datos, manejo de error, useState, useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,52 @@
 import { useEffect, useState } from "react"
+import './App.css'
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 //const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
+const CAT_PREFIX_IMG_URL = 'https://cataas.com'
 
 export function App () {
   const [fact, setFact] = useState()
   const [imageUrl, setImageUrl] = useState()
+  const [factError, setFactError] = useState()
 
 //useEffect(()=>{}, [])
 
+//efecto para recuperar la frase al renderizar la pagina 
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
-    .then(res => res.json())
+    .then(res => {
+      if(!res.ok){
+        setFactError('No se ha podido recuperar la frase')
+      }
+      return  res.json()
+    })
     .then(data => {
       const {fact} = data
       setFact(fact)
+    })
+  }, [])
 
-      const firstWord = fact.split(' ', 3)
+//para recuperar la imagen cada vez que tenemos una frase nueva
+  useEffect(() => {
+    if(!fact) return
+
+    const firstWord = fact.split(' ', 3)
 
       fetch(`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`)
         .then(res => res.json())
         .then(response => {
           const {url} = response
-          setImageUrl(`https://cataas.com${url}`)
+          setImageUrl(url)
         }) 
-    })
-  }, [])
+  }, [fact])
 
 
   return (
     <main>
       <h1> App de gatos </h1>
-      {fact && <p>{fact}</p>}
-      {imageUrl && <img src={imageUrl} alt={`Image extracted using the first three 
-      word for ${fact} `} />}
+        {fact && <p>{fact}</p>}
+        {imageUrl && <img src={`${CAT_PREFIX_IMG_URL}${imageUrl}`} alt={`Image extracted using the first three 
+        word for ${fact} `} />} 
     </main>  
   ) }
